2c56b99f2633dd1c77b37205bbddd9163300bbc5
Jakub Koza <jakub.koza13@gmail.com>
2021-03-10 19:50:21 +0100

    Variables and logical expressions

--- kotlin.kt
+++ kotlin.kt
@@ -1,3 +1,23 @@
 fun main(){
-    println("XDS");
+    val name: String = "Jakub"
+    println(name)
+
+    var prop1: Int? = 27
+    if (prop1 == null){
+        println("NULL")
+    }
+    else{
+        println("NOT NULL")
+    }
+
+    when(prop1){
+        25 -> println("Hi")
+        26 -> println("There")
+        else -> println("ERROR")
+    }
+
+    prop1 = null
+    var prop2: Int = if(prop1 != null) prop1 else 100
+
+    println(prop2)
 }
